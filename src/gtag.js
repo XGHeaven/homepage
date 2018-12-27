775d932946d1739891bdaa8983267f96fec2df9f
XGHeaven <xgheaven@gmail.com>
2018-12-27 22:25:32 +0800

    fix: build error

--- src/gtag.js
+++ src/gtag.js
@@ -1,7 +1,10 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
-if (process.env.NODE_ENV !== 'production') {
-  return
-}
-gtag('js', new Date());
-gtag('config', 'UA-71388235-2');
+
+(function() {
+  if (process.env.NODE_ENV !== 'production') {
+    return
+  }
+  gtag('js', new Date());
+  gtag('config', 'UA-71388235-2');
+})()
