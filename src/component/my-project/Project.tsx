b924899f3fc3d2524c1e63f2bc57bec03604478d
XGHeaven <xgheaven@gmail.com>
2018-12-27 22:16:13 +0800

    feat: add project

--- src/component/my-project/Project.tsx
+++ src/component/my-project/Project.tsx
@@ -1,14 +1,67 @@
 import * as React from 'react'
+import styled, { keyframes } from 'react-emotion';
+
+const LinkContainer = styled.a`
+  display: block;
+  text-decoration: none;
+  color: inherit;
+  margin-bottom: 18px;
+`
+
+const ProjectContainer = styled.div`
+  text-align: center;
+  padding: 36px 0 24px 0;
+  box-shadow: 0 0 2px rgba(255, 255, 255, .3);
+  transition: all 0.3s ease-in-out;
+  border-radius: 12px;
+  position: relative;
+
+  &:hover {
+    box-shadow: 0 0 6px rgba(255, 255, 255, .6);
+
+    &::before {
+      border-top-color: white;
+    }
+  }
+
+  &::before {
+    content: '';
+    position: absolute;
+    transition: all .3s;
+    top: 18px;
+    left: 0;
+    right: 0;
+    margin: 0 auto;
+    width: 0;
+    height: 0;
+    border: 6px solid rgba(255, 255, 255, .5);
+    border-bottom: 0;
+    border-left-color: transparent;
+    border-right-color: transparent;
+  }
+`
+
+const ProjectTitle = styled.div`
+  font-size: 24px;
+`
+
+const ProjectDescription = styled.div`
+`
 
 export default function Project(props: {
   title: string,
-  name: string,
   img?: string,
+  href?: string,
   children?: React.ReactNode
 }) {
-  return (
-    <div>
+  const { title, children, href } = props
 
-    </div>
+  return (
+    <LinkContainer href={href || 'javascript:void 0;'} rel="noopener" target="_blank">
+      <ProjectContainer>
+        <ProjectTitle>{title}</ProjectTitle>
+        <ProjectDescription>{children}</ProjectDescription>
+      </ProjectContainer>
+    </LinkContainer>
   )
 }
--- src/gtag.js
+++ src/gtag.js
@@ -1,4 +1,7 @@
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
+if (process.env.NODE_ENV !== 'production') {
+  return
+}
 gtag('js', new Date());
 gtag('config', 'UA-71388235-2');
